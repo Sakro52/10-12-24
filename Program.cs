568531424e11bcc69599b217dde7512e5151cf7d
Kab-35-16 <Kab-35-16@KAB-32-10->
2024-12-10 13:38:12 +0200

    234234353

--- Program.cs
+++ Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace _10_12_24
 {
+    /// <summary>
+    /// Отсортировать массив по взростанию и убыванию 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach(var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Array.Reverse(people);
+            foreach (var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadLine();
         }
     }
 }
